6880d15470dc85a39ca20cf3b825ef4fb2434894
unknown <harunzsln0@gmail.com>
2025-08-14 17:52:04 +0300

    weather model created

---
+++ havadurumu/lib/models/weather_model.dart
@@ -0,0 +1,34 @@
+class WeatherModel {
+  final String city;
+  final String gun;
+  final String ikon;
+  final String durum;
+  final String derece;
+  final String min;
+  final String max;
+  final String gece;
+  final String nem;
+
+  WeatherModel(
+    this.city,
+    this.gun,
+    this.ikon,
+    this.durum,
+    this.derece,
+    this.min,
+    this.max,
+    this.gece,
+    this.nem,
+  );
+
+  WeatherModel.fromJson(Map<String, dynamic> json)
+    : city = json['city'],
+      gun = json['day'],
+      ikon = json['icon'],
+      durum = json['description'],
+      derece = json['degree'],
+      min = json['min'],
+      max = json['max'],
+      gece = json['night'],
+      nem = json['humidity'];
+}
